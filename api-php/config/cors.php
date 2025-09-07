227d7982e98d5b0e6c1a3d0878702c16372b0a1b
Pedrohmedd <pedromedeirosetec02@gmail.com>
2025-09-07 16:45:10 -0300

    cors options

--- api-php/config/cors.php
+++ api-php/config/cors.php
@@ -2,4 +2,9 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 ?>
