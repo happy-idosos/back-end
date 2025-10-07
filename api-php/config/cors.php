b9fd38f983cd3cdb1dfc6c0c70b8eadb201dce29
pedrohmedd <pedromedeirosetec02@gmail.com>
2025-10-07 11:00:48 -0300

    cors chapacao

--- api-php/config/cors.php
+++ api-php/config/cors.php
@@ -1,7 +1,7 @@
 <?php
 // Headers CORS para desenvolvimento
 header("Access-Control-Allow-Origin: http://localhost:5173"); // URL do seu React
-header("Access-Control-Allow-Origin: *"); // Permitir todas as origens (para desenvolvimento)
+//header("Access-Control-Allow-Origin: *"); // Permitir todas as origens (para desenvolvimento)
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Credentials: true");
