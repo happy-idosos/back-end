9b001e1fd42a13ffe698b459a9b41250b8c2b97d
Pedrohmedd <pedromedeirosetec02@gmail.com>
2025-10-14 03:37:03 -0300

    cors novo

--- api-php/config/cors.php
+++ api-php/config/cors.php
@@ -1,8 +1,6 @@
 <?php
 // Headers CORS para desenvolvimento
 header("Access-Control-Allow-Origin: www.happyidosos.com.br"); // URL do seu front-end em produção
-//header("Access-Control-Allow-Origin: http://localhost:5173"); // URL do seu React
-//header("Access-Control-Allow-Origin: *"); // Permitir todas as origens (para desenvolvimento)
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Credentials: true");
