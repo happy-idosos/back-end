7c2edba041b20726ddc6d82c23cc3e415d359fd1
Pedrohmedd <pedromedeirosetec02@gmail.com>
2025-10-14 03:42:01 -0300

    cors

--- api-php/config/cors.php
+++ api-php/config/cors.php
@@ -1,6 +1,6 @@
 <?php
 // Headers CORS para desenvolvimento
-header("Access-Control-Allow-Origin: www.happyidosos.com.br"); // URL do seu front-end em produção
+header("Access-Control-Allow-Origin: https://www.happyidosos.com.br");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Credentials: true");
--- api-php/index.php
+++ api-php/index.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: www.happyidosos.com.br"); // URL do seu front-end em produção
+header("Access-Control-Allow-Origin: https://www.happyidosos.com.br");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
